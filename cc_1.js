9cd3baeeada547930c4da13d483affc9019fc101
Myia Lamy <mlamy@usf.edu>
2024-08-31 14:02:01 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -10,3 +10,13 @@ console.log(isActive, typeof(isActive));
 
 
 
+// Task 2: Compound Data Types
+
+let products = ["Airpods, Beats Pill, Sunglasses"];
+const productDetails = {
+    name: products, 
+    price: 150.00,
+    inStock: 100,
+} 
+
+console.log(products, productDetails);
