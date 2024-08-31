573c4301f8af13f7fea1116805d997bdbc9967bd
Myia Lamy <mlamy@usf.edu>
2024-08-31 13:39:33 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,12 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Cameron Johnson";
+const employeeID = 21;
+var isActive = true; 
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
+
+
+
